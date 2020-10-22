6bed9dd0b9cafd9c4badc735f40f288f1cb3db77
faysvas@yahoo.com <faysvas@yahoo.com>
2020-10-22 19:15:48 +0300

    Add a few basic tests

--- test/mist-form.test.js
+++ test/mist-form.test.js
@@ -1,38 +1,48 @@
-import { html, fixture, expect } from '@open-wc/testing';
-
+import { html, fixture, expect, waitUntil } from '@open-wc/testing';
+import '@polymer/paper-styles/typography.js';
+import '@polymer/paper-styles/default-theme.js';
+import '@polymer/paper-styles/shadow.js';
+import '@polymer/paper-styles/demo-pages.js';
+import '@polymer/paper-styles/color.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-input/paper-textarea.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-radio-group/paper-radio-group.js';
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-toggle-button/paper-toggle-button.js';
+import '@polymer/paper-spinner/paper-spinner.js';
+import '@polymer/iron-ajax/iron-ajax.js';
 import '../mist-form.js';
 
 describe('MistForm', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture(html`
-      <mist-form></mist-form>
-    `);
-
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+  it("Displays error message when you don't pass a json url", async () => {
+    const el = await fixture(html` <mist-form></mist-form> `);
+    await waitUntil(() => el.dataError);
+    expect(el).shadowDom.to.equal(
+      "We couldn't load the form. Please try again"
+    );
   });
 
-  it('increases the counter on button click', async () => {
-    const el = await fixture(html`
-      <mist-form></mist-form>
-    `);
-    el.shadowRoot.querySelector('button').click();
-
-    expect(el.counter).to.equal(6);
-  });
-
-  it('can override the title via attribute', async () => {
-    const el = await fixture(html`
-      <mist-form title="attribute title"></mist-form>
-    `);
-
-    expect(el.title).to.equal('attribute title');
+  it('Displays a single input field when you pass inputField.json', async () => {
+    const el = await fixture(
+      html`<mist-form
+        id=${'test-mist-form'}
+        .src=${'/base/test/inputField.json'}
+      ></mist-form> `
+    );
+    await waitUntil(() => el.data);
+    // expect(el).shadowDom.to.equal(
+    //   "We couldn't load the form. Please try again"
+    // );
+    expect(el).lightDom.to.equal(
+      "<mist-form>We couldn't load the form. Please try again</mist-form>"
+    );
   });
 
   it('passes the a11y audit', async () => {
-    const el = await fixture(html`
-      <mist-form></mist-form>
-    `);
+    const el = await fixture(html` <mist-form></mist-form> `);
 
     await expect(el).shadowDom.to.be.accessible();
   });
