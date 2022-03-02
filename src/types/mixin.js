09bac9dfb5b5670f4017ab84043295fc9fbc0856
Dimitris Moraitis <dimo@mist.io>
2022-03-02 16:38:50 +0200

    Support email & password formats

--- src/types/mixin.js
+++ src/types/mixin.js
@@ -13,31 +13,42 @@ export const fieldMixin = superClass =>
     static get properties() {
       return {
         spec: {
-          type: Object
-        }
-      }
+          type: Object,
+        },
+      };
     }
 
     get domValue() {
       const field = this.shadowRoot.querySelector('.mist-form-field');
       if (this.widget === 'select') {
-        if (field.querySelector("vaadin-item[selected]")) {
-          return this.cast(field.querySelector("vaadin-item[selected]").value);
+        if (field.querySelector('vaadin-item[selected]')) {
+          return this.cast(field.querySelector('vaadin-item[selected]').value);
         }
-        field._menuElement.querySelectorAll('vaadin-item').forEach((i,j) => {if (i.getAttribute('value') == field.value) field._menuElement.selected = j});
+        field._menuElement.querySelectorAll('vaadin-item').forEach((i, j) => {
+          if (i.getAttribute('value') === field.value)
+            field._menuElement.selected = j;
+        });
       }
-      return this.cast(field.value !== undefined ? field.value : field.getAttribute('value'));
+      return this.cast(
+        field.value !== undefined ? field.value : field.getAttribute('value')
+      );
     }
 
     get widget() {
-      if (this.spec.uiSchema && this.spec.uiSchema["ui:widget"] && this.spec.uiSchema["ui:widget"]) {
-        return this.spec.uiSchema["ui:widget"];
+      if (
+        this.spec.uiSchema &&
+        this.spec.uiSchema['ui:widget'] &&
+        this.spec.uiSchema['ui:widget']
+      ) {
+        return this.spec.uiSchema['ui:widget'];
       }
       return '';
     }
 
     get step() {
-      return this.spec.jsonSchema.multipleOf ? Number(this.spec.jsonSchema.multipleOf) : undefined;
+      return this.spec.jsonSchema.multipleOf
+        ? Number(this.spec.jsonSchema.multipleOf)
+        : undefined;
     }
 
     get parent() {
@@ -49,9 +60,9 @@ export const fieldMixin = superClass =>
         el = el.parentNode;
       }
       while (el.host) {
-        el = el.host
+        el = el.host;
       }
-      const {parent} = el;
+      const { parent } = el;
       return parent || el;
     }
 
@@ -60,7 +71,10 @@ export const fieldMixin = superClass =>
     }
 
     get isReadOnly() {
-      return Boolean((this.spec.uiSchema && this.spec.uiSchema['ui:readonly']) || this.spec.jsonSchema.readOnly)
+      return Boolean(
+        (this.spec.uiSchema && this.spec.uiSchema['ui:readonly']) ||
+          this.spec.jsonSchema.readOnly
+      );
     }
 
     get isDisabled() {
@@ -72,25 +86,25 @@ export const fieldMixin = superClass =>
     }
 
     get placeholder() {
-      return this.spec.uiSchema && this.spec.uiSchema["ui:placeholder"];
+      return this.spec.uiSchema && this.spec.uiSchema['ui:placeholder'];
     }
 
     get items() {
       const ret = [];
       if (this.spec.jsonSchema.enum) {
-          this.spec.jsonSchema.enum.forEach(element => {
-              ret.push({
-                  label: String(element),
-                  value: String(element)
-              })
+        this.spec.jsonSchema.enum.forEach(element => {
+          ret.push({
+            label: String(element),
+            value: String(element),
           });
+        });
       } else if (this.spec.jsonSchema.examples) {
-          this.spec.jsonSchema.examples.forEach(element => {
-              ret.push({
-                  label: element,
-                  value: element
-              })
+        this.spec.jsonSchema.examples.forEach(element => {
+          ret.push({
+            label: element,
+            value: element,
           });
+        });
       }
       return ret;
     }
@@ -104,10 +118,10 @@ export const fieldMixin = superClass =>
       console.debug(this, 'Updating form field', e);
       const valueChangedEvent = new CustomEvent('field-value-changed', {
         detail: {
-          id: this.spec.id
+          id: this.spec.id,
         },
         bubbles: false,
-        composed: true
+        composed: true,
       });
       this.dispatchEvent(valueChangedEvent);
     }
@@ -135,7 +149,7 @@ export const fieldMixin = superClass =>
       this.importWidgets();
 
       if (this.spec.jsonSchema.enum) {
-        let val = this.domValue;
+        const val = this.domValue;
         if (this.spec.jsonSchema.enum.indexOf(val) === -1) {
           this.requestUpdate();
         }
@@ -147,14 +161,14 @@ export const fieldMixin = superClass =>
         case 'textarea':
           if (customElements.get('vaadin-text-area') === undefined) {
             import('@vaadin/text-area').then(() => {
-              console.debug('imported vaadin textarea')
+              console.debug('imported vaadin textarea');
             });
           }
           break;
         case 'checkboxes':
           if (customElements.get('vaadin-checkbox') === undefined) {
             import('@vaadin/checkbox').then(() => {
-              console.debug('imported vaadin checkbox')
+              console.debug('imported vaadin checkbox');
             });
           }
           break;
@@ -164,40 +178,60 @@ export const fieldMixin = superClass =>
               console.debug('imported vaadin select');
             });
             import('@vaadin/item').then(() => {
-              console.debug('imported vaadin item')
+              console.debug('imported vaadin item');
             });
             import('@vaadin/list-box').then(() => {
-              console.debug('imported vaadin list-box')
+              console.debug('imported vaadin list-box');
             });
           }
           break;
         case 'combo-box':
           if (customElements.get('vaadin-combo-box') === undefined) {
             import('@vaadin/combo-box').then(() => {
-              console.debug('imported vaadin combo box')
+              console.debug('imported vaadin combo box');
             });
           }
           break;
         case 'text-field':
           if (customElements.get('vaadin-text-field') === undefined) {
             import('@vaadin/text-field').then(() => {
-              console.debug('imported vaadin text-field')
+              console.debug('imported vaadin text-field');
+            });
+          }
+          break;
+        case 'password':
+          if (customElements.get('vaadin-password-field') === undefined) {
+            import('@vaadin/password-field').then(() => {
+              console.debug('imported vaadin password-field');
+            });
+          }
+          break;
+        case 'email':
+          if (customElements.get('vaadin-email-field') === undefined) {
+            import('@vaadin/email-field').then(() => {
+              console.debug('imported vaadin email-field');
             });
           }
           break;
         case 'radio':
           if (customElements.get('vaadin-radio-group') === undefined) {
-            import('@vaadin/radio-group/theme/material/vaadin-radio-group.js').then(console.debug('imported vaadin radio group'));
+            import(
+              '@vaadin/radio-group/theme/material/vaadin-radio-group.js'
+            ).then(console.debug('imported vaadin radio group'));
           }
           break;
         case 'number':
           if (customElements.get('vaadin-number-field') === undefined) {
-            import('@vaadin/number-field').then(console.debug('imported vaadin number field'));
+            import('@vaadin/number-field').then(
+              console.debug('imported vaadin number field')
+            );
           }
           break;
         case 'integer':
           if (customElements.get('vaadin-integer-field') === undefined) {
-            import('@vaadin/integer-field').then(console.debug('imported vaadin integer field'));
+            import('@vaadin/integer-field').then(
+              console.debug('imported vaadin integer field')
+            );
           }
           break;
         case 'range':
@@ -206,4 +240,4 @@ export const fieldMixin = superClass =>
           break;
       }
     }
-  }
+  };
--- src/types/string.js
+++ src/types/string.js
@@ -1,177 +1,270 @@
 import { html, css, LitElement, render } from 'lit';
-import {ifDefined} from 'lit/directives/if-defined.js';
-import {repeat} from 'lit/directives/repeat.js';
-import {fieldMixin} from './mixin.js';
-
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { repeat } from 'lit/directives/repeat.js';
+import { fieldMixin } from './mixin.js';
+import '@vaadin/icons';
+import '@vaadin/icon';
 
 export class MistFormStringField extends fieldMixin(LitElement) {
-    static get properties() {
-        return {
-            value: { type: String }
-        }
-    }
+  static get properties() {
+    return {
+      value: { type: String },
+    };
+  }
 
-    static get styles() {
-        return [
-            css``,
-        ];
-    }
+  static get styles() {
+    return [css``];
+  }
 
-    get widget() {
-        return super.widget || (this.spec.jsonSchema.examples ? 'combo-box' : (
-            this.spec.jsonSchema.enum && 'select' || 'text-field'));
-    }
+  get widget() {
+    return (
+      super.widget ||
+      (this.spec.jsonSchema.examples
+        ? 'combo-box'
+        : (this.spec.jsonSchema.enum && 'select') ||
+          (this.spec.jsonSchema.format === 'email' && 'email') ||
+          'text-field')
+    );
+  }
 
-    render() {
-        if (!this.spec) return html``
-        if (this.widget === 'textarea') {
-            return html`
-            <vaadin-text-area
-                has-controls clear-button-visible
-                ?required="${this.spec.jsonSchema.required}"
-                value="${ifDefined(this.spec.formData)}"
-                label="${this.spec.jsonSchema.title}"
-                helper-text="${ifDefined(this.spec.jsonSchema.description)}"
-                class="${this.spec.classes || ''} mist-form-field"
-                @value-changed=${this.debouncedEventChange}
-                minlength=${ifDefined(this.spec.jsonSchema.minLength)}
-                maxlength=${ifDefined(this.spec.jsonSchema.maxLength)}
-                pattern=${ifDefined(this.spec.jsonSchema.pattern)}
-                colspan=${ifDefined(this.spec.jsonSchema.colspan)}
-                placeholder="${ifDefined(this.placeholder)}"
-                ?autofocus=${this.hasAutoFocus}
-                ?disabled=${this.isDisabled}
-                ?readonly=${this.isReadOnly}
-                ?hidden=${this.isHidden}
-            >
-                ${this.icon || ''}
-            </vaadin-text-area>`;    
-        }
-        if (this.spec.jsonSchema.enum) {
-            if (this.widget === 'checkboxes') {
-                return html`
-                    <vaadin-checkbox-group
-                        label="${this.spec.jsonSchema.title}"
-                        @value-changed="${(e) => {this.value = e.detail.value}}"
-                        class="${this.spec.classes || ''} mist-form-field"
-                        ?autofocus=${this.hasAutoFocus}
-                        ?disabled=${this.isDisabled}
-                        ?readonly=${this.isReadOnly}
-                        ?hidden=${this.isHidden}
-                        theme="vertical">
-                    ${repeat(this.spec.jsonSchema.enum || [], (item) => (item), (item) => html`
-                        <vaadin-checkbox
-                            id="${item}"
-                            label="${item}"
-                            ?checked=${this.spec.formData && this.spec.formData.indexOf(item) !== -1}
-                            @change=${e => {
-                                e.detail = {id: item, value:e.target.checked};
-                                this.debouncedEventChange(e);
-                            }}
-                        >
-                        </vaadin-checkbox>
-                    `)}
-                    </vaadin-checkbox-group>
-                `
-            } if (this.widget === 'radio') {
-                return html`
-                    <vaadin-radio-group
-                        ?required="${this.spec.jsonSchema.required}"
-                        label="${this.spec.jsonSchema.title}"
-                        helper-text="${ifDefined(this.spec.jsonSchema.description)}"
-                        class="${this.spec.classes || ''} mist-form-field"
-                        @value-changed=${this.debouncedEventChange}
-                        ?autofocus=${this.hasAutoFocus}
-                        ?disabled=${this.isDisabled}
-                        ?readonly=${this.isReadOnly}
-                        ?hidden=${this.isHidden}
-                        value=${this.cast(this.spec.formData)}
-                    >
-                        ${this.items.map(
-                            (item) => html`
-                                <vaadin-radio-button value="${item.value}" label="${item.label}" ?checked=${String(Boolean(this.formData)) === String(item.value)}>
-                                </vaadin-radio-button>
-                            `
-                        )}
-                    </vaadin-radio-group>
-                `
-            }
-            return html`
-                <vaadin-select
-                    .renderer="${this.renderer}"
-                    ?required="${this.spec.jsonSchema.required}"
-                    .items="${this.items}"
-                    .value="${this.spec.formData}"
-                    label="${this.spec.jsonSchema.title}"
-                    helper-text="${ifDefined(this.spec.jsonSchema.description)}"
-                    class="${this.spec.classes || ''} mist-form-field"
-                    @value-changed=${this.debouncedEventChange}
-                    placeholder="${ifDefined(this.placeholder)}"
-                    ?autofocus=${this.hasAutoFocus}
-                    ?disabled=${this.isDisabled}
-                    ?readonly=${this.isReadOnly}
-                    ?hidden=${this.isHidden}
-                ></vaadin-select>`
-        } if (this.spec.jsonSchema.examples){
-            return html`
-            <vaadin-combo-box
-                allow-custom-value
-                item-label-path="label"
-                item-value-path="value"
-                ?required="${this.spec.jsonSchema.required}"
-                .items="${this.items}"
-                .value="${this.spec.formData || ''}"
-                label="${this.spec.jsonSchema.title}"
-                helper-text="${ifDefined(this.spec.jsonSchema.description)}"
-                class="${this.spec.classes || ''} mist-form-field"
-                @value-changed=${this.debouncedEventChange}
-                placeholder="${ifDefined(this.placeholder)}"
-                ?autofocus=${this.hasAutoFocus}
-                ?disabled=${this.isDisabled}
-                ?readonly=${this.isReadOnly}
-                ?hidden=${this.isHidden}
-            ></vaadin-combo-box>`
-        }
+  get pattern() {
+    let ret;
+    if (this.spec.jsonSchema.format === 'email') {
+      ret = '^([a-zA-Z0-9_\\.\\-+])+@[a-zA-Z0-9-.]+\\.[a-zA-Z0-9-]{2,}$';
+    } else if (this.spec.jsonSchema.format === 'uri') {
+      ret = '^(([a-zA-Z0-9_\\.\\-+])+://)?[a-zA-Z0-9-.]+\\.[a-zA-Z0-9-]{2,}$';
+    }
+    return this.spec.jsonSchema.pattern || ret;
+  }
+
+  render() {
+    if (!this.spec) return html``;
+    if (this.widget === 'textarea') {
+      return html` <vaadin-text-area
+        has-controls
+        clear-button-visible
+        ?required="${this.spec.jsonSchema.required}"
+        value="${ifDefined(this.spec.formData)}"
+        label="${this.spec.jsonSchema.title}"
+        helper-text="${ifDefined(this.spec.jsonSchema.description)}"
+        class="${this.spec.classes || ''} mist-form-field"
+        @value-changed=${this.debouncedEventChange}
+        minlength=${ifDefined(this.spec.jsonSchema.minLength)}
+        maxlength=${ifDefined(this.spec.jsonSchema.maxLength)}
+        pattern=${ifDefined(this.spec.jsonSchema.pattern)}
+        colspan=${ifDefined(this.spec.jsonSchema.colspan)}
+        placeholder="${ifDefined(this.placeholder)}"
+        ?autofocus=${this.hasAutoFocus}
+        ?disabled=${this.isDisabled}
+        ?readonly=${this.isReadOnly}
+        ?hidden=${this.isHidden}
+        .pattern=${this.pattern}
+      >
+        ${this.icon || ''}
+      </vaadin-text-area>`;
+    }
+    if (this.spec.jsonSchema.enum) {
+      if (this.widget === 'checkboxes') {
         return html`
-            <vaadin-text-field
-                has-controls clear-button-visible
-                ?required="${this.spec.jsonSchema.required}"
-                value="${ifDefined(this.spec.formData)}"
-                label="${this.spec.jsonSchema.title || (Number.isNaN(Number(this.spec.id)) && this.spec.id) || ''}"
-                helper-text="${ifDefined(this.spec.jsonSchema.description)}"
-                class="${this.spec.classes || ''} mist-form-field"
-                @value-changed=${this.debouncedEventChange}
-                minlength=${ifDefined(this.spec.jsonSchema.minLength)}
-                maxlength=${ifDefined(this.spec.jsonSchema.maxLength)}
-                pattern=${ifDefined(this.spec.jsonSchema.pattern)}
-                colspan=${ifDefined(this.spec.jsonSchema.colspan)}
-                autocomplete=${this.spec.uiSchema["ui:autocomplete"]}
-                placeholder="${ifDefined(this.placeholder)}"
-                ?autofocus=${this.hasAutoFocus}
-                ?disabled=${this.isDisabled}
-                ?readonly=${this.isReadOnly}
-                ?hidden=${this.isHidden}
-            >
-            </vaadin-text-field>`;
+          <vaadin-checkbox-group
+            label="${this.spec.jsonSchema.title}"
+            @value-changed="${e => {
+              this.value = e.detail.value;
+            }}"
+            class="${this.spec.classes || ''} mist-form-field"
+            ?autofocus=${this.hasAutoFocus}
+            ?disabled=${this.isDisabled}
+            ?readonly=${this.isReadOnly}
+            ?hidden=${this.isHidden}
+            theme="vertical"
+          >
+            ${repeat(
+              this.spec.jsonSchema.enum || [],
+              item => item,
+              item => html`
+                <vaadin-checkbox
+                  id="${item}"
+                  label="${item}"
+                  ?checked=${this.spec.formData &&
+                  this.spec.formData.indexOf(item) !== -1}
+                  @change=${e => {
+                    e.detail = { id: item, value: e.target.checked };
+                    this.debouncedEventChange(e);
+                  }}
+                >
+                </vaadin-checkbox>
+              `
+            )}
+          </vaadin-checkbox-group>
+        `;
+      }
+      if (this.widget === 'radio') {
+        return html`
+          <vaadin-radio-group
+            ?required="${this.spec.jsonSchema.required}"
+            label="${this.spec.jsonSchema.title}"
+            helper-text="${ifDefined(this.spec.jsonSchema.description)}"
+            class="${this.spec.classes || ''} mist-form-field"
+            @value-changed=${this.debouncedEventChange}
+            ?autofocus=${this.hasAutoFocus}
+            ?disabled=${this.isDisabled}
+            ?readonly=${this.isReadOnly}
+            ?hidden=${this.isHidden}
+            value=${this.cast(this.spec.formData)}
+          >
+            ${this.items.map(
+              item => html`
+                <vaadin-radio-button
+                  value="${item.value}"
+                  label="${item.label}"
+                  ?checked=${String(Boolean(this.formData)) ===
+                  String(item.value)}
+                >
+                </vaadin-radio-button>
+              `
+            )}
+          </vaadin-radio-group>
+        `;
+      }
+      return html` <vaadin-select
+        .renderer="${this.renderer}"
+        ?required="${this.spec.jsonSchema.required}"
+        .items="${this.items}"
+        .value="${this.spec.formData}"
+        label="${this.spec.jsonSchema.title}"
+        helper-text="${ifDefined(this.spec.jsonSchema.description)}"
+        class="${this.spec.classes || ''} mist-form-field"
+        @value-changed=${this.debouncedEventChange}
+        placeholder="${ifDefined(this.placeholder)}"
+        ?autofocus=${this.hasAutoFocus}
+        ?disabled=${this.isDisabled}
+        ?readonly=${this.isReadOnly}
+        ?hidden=${this.isHidden}
+        .pattern=${this.pattern}
+      ></vaadin-select>`;
     }
+    if (this.spec.jsonSchema.examples) {
+      return html` <vaadin-combo-box
+        allow-custom-value
+        item-label-path="label"
+        item-value-path="value"
+        ?required="${this.spec.jsonSchema.required}"
+        .items="${this.items}"
+        .value="${this.spec.formData || ''}"
+        label="${this.spec.jsonSchema.title}"
+        helper-text="${ifDefined(this.spec.jsonSchema.description)}"
+        class="${this.spec.classes || ''} mist-form-field"
+        @value-changed=${this.debouncedEventChange}
+        placeholder="${ifDefined(this.placeholder)}"
+        ?autofocus=${this.hasAutoFocus}
+        ?disabled=${this.isDisabled}
+        ?readonly=${this.isReadOnly}
+        ?hidden=${this.isHidden}
+        .pattern=${this.pattern}
+      ></vaadin-combo-box>`;
+    }
+    if (this.widget === 'password') {
+      return html` <vaadin-password-field
+        has-controls
+        clear-button-visible
+        ?required="${this.spec.jsonSchema.required}"
+        value="${ifDefined(this.spec.formData)}"
+        label="${this.spec.jsonSchema.title ||
+        (Number.isNaN(Number(this.spec.id)) && this.spec.id) ||
+        ''}"
+        helper-text="${ifDefined(this.spec.jsonSchema.description)}"
+        class="${this.spec.classes || ''} mist-form-field"
+        @value-changed=${this.debouncedEventChange}
+        minlength=${ifDefined(this.spec.jsonSchema.minLength)}
+        maxlength=${ifDefined(this.spec.jsonSchema.maxLength)}
+        pattern=${ifDefined(this.spec.jsonSchema.pattern)}
+        colspan=${ifDefined(this.spec.jsonSchema.colspan)}
+        autocomplete=${this.spec.uiSchema['ui:autocomplete']}
+        placeholder="${ifDefined(this.placeholder)}"
+        ?autofocus=${this.hasAutoFocus}
+        ?disabled=${this.isDisabled}
+        ?readonly=${this.isReadOnly}
+        ?hidden=${this.isHidden}
+        .pattern=${this.pattern}
+      >
+      </vaadin-password-field>`;
+    }
+    if (this.widget === 'email') {
+      return html` <vaadin-email-field
+        has-controls
+        clear-button-visible
+        ?required="${this.spec.jsonSchema.required}"
+        value="${ifDefined(this.spec.formData)}"
+        label="${this.spec.jsonSchema.title ||
+        (Number.isNaN(Number(this.spec.id)) && this.spec.id) ||
+        ''}"
+        helper-text="${ifDefined(this.spec.jsonSchema.description)}"
+        class="${this.spec.classes || ''} mist-form-field"
+        @value-changed=${this.debouncedEventChange}
+        minlength=${ifDefined(this.spec.jsonSchema.minLength)}
+        maxlength=${ifDefined(this.spec.jsonSchema.maxLength)}
+        pattern=${ifDefined(this.spec.jsonSchema.pattern)}
+        colspan=${ifDefined(this.spec.jsonSchema.colspan)}
+        autocomplete=${this.spec.uiSchema['ui:autocomplete']}
+        placeholder="${ifDefined(this.placeholder)}"
+        ?autofocus=${this.hasAutoFocus}
+        ?disabled=${this.isDisabled}
+        ?readonly=${this.isReadOnly}
+        ?hidden=${this.isHidden}
+        .pattern=${this.pattern}
+      >
+      </vaadin-email-field>`;
+    }
+    return html` <vaadin-text-field
+      has-controls
+      clear-button-visible
+      ?required="${this.spec.jsonSchema.required}"
+      value="${ifDefined(this.spec.formData)}"
+      label="${this.spec.jsonSchema.title ||
+      (Number.isNaN(Number(this.spec.id)) && this.spec.id) ||
+      ''}"
+      helper-text="${ifDefined(this.spec.jsonSchema.description)}"
+      class="${this.spec.classes || ''} mist-form-field"
+      @value-changed=${this.debouncedEventChange}
+      minlength=${ifDefined(this.spec.jsonSchema.minLength)}
+      maxlength=${ifDefined(this.spec.jsonSchema.maxLength)}
+      pattern=${ifDefined(this.spec.jsonSchema.pattern)}
+      colspan=${ifDefined(this.spec.jsonSchema.colspan)}
+      autocomplete=${this.spec.uiSchema['ui:autocomplete']}
+      placeholder="${ifDefined(this.placeholder)}"
+      ?autofocus=${this.hasAutoFocus}
+      ?disabled=${this.isDisabled}
+      ?readonly=${this.isReadOnly}
+      ?hidden=${this.isHidden}
+      .pattern=${this.pattern}
+    >
+    </vaadin-text-field>`;
+  }
 
-    cast(value) {
-        if (value === undefined && this.value !== undefined) {
-            return this.cast(this.value);
-        }
-        return String(value || '');
+  cast(value) {
+    if (value === undefined && this.value !== undefined) {
+      return this.cast(this.value);
     }
+    return String(value || '');
+  }
 
-    renderer(root) {
-        render(
-            html`
-                <vaadin-list-box>
-                ${this.items ? this.items.map((item) => html`<vaadin-item value="${item.value}">${item.label}</vaadin-item>`) : ''}
-                </vaadin-list-box>
-          `,
-          root
-        )
-    }
+  renderer(root) {
+    render(
+      html`
+        <vaadin-list-box>
+          ${this.items
+            ? this.items.map(
+                item =>
+                  html`<vaadin-item value="${item.value}"
+                    >${item.label}</vaadin-item
+                  >`
+              )
+            : ''}
+        </vaadin-list-box>
+      `,
+      root
+    );
+  }
 }
 
-customElements.define('mist-form-string-field', MistFormStringField)
+customElements.define('mist-form-string-field', MistFormStringField);
