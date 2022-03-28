e55d3e62ddea54f757a55df4349154458a40ecd8
Eis <monoatomic.alex@gmail.com>
2022-03-28 14:18:05 +0300

    Rename and fix tests, fix npm test script to run the cypress tests

---
+++ cypress/integration/test_arrays_form.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+/* eslint-disable consistent-return */
+
+// Arrays List of Fixed Items is buggy.
+
+describe('Unit Test Arrays', () => {
+  // ignore chrome ResizeObserver error
+  Cypress.on('uncaught:exception', e => {
+    if (e.message.includes('ResizeObserver loop limit exceeded')) return false;
+  });
+
+  it('Opens playgroung', () => {
+    cy.visit('http://localhost:8000/demo/index.html');
+  });
+
+  it('Select subform', () => {
+    cy.get('vaadin-tabs > vaadin-tab').then(items => {
+      cy.wrap(items[3]).click().should('have.attr', 'selected');
+    });
+  });
+});
